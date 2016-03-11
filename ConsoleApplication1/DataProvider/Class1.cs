1a274d4ff765590ccac7e7146935eef416fcc4c4
jango2015 <jango2015>
2016-03-11 15:38:31 +0800

    bin

---
+++ ConsoleApplication1/DataProvider/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace DataProvider
+{
+
+    public class meituanJsons
+    {
+        public ObjectId _id { get; set; }
+        public string Name { get; set; }
+
+        public string JsonStr { get; set; }
+    }
+    class Class1
+    {
+    }
+}
